b4e05ea2c1bcff801d820c2d2fb02bc74a75f168
Jiro Matsuzawa <matsuzawa.jr@gmail.com>
2022-11-04 21:53:56 +0900

    add: Goodby()
    The test fails on purpose to make github actions fail

--- main_test.go
+++ main_test.go
@@ -12,3 +12,14 @@ func TestHello(t *testing.T) {
 		t.Errorf("actual=%q, expected=%q", actual, expected)
 	}
 }
+
+func TestGoodby(t *testing.T) {
+	// Setup
+	expected := "Goodby World"
+	// Exercise
+	actual := Goodby("World")
+	// Verify
+	if actual != expected {
+		t.Errorf("actual=%q, expected=%q", actual, expected)
+	}
+}
